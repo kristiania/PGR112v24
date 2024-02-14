2d196d8ae06164ec55b2692b2ebfa701b2cb621d
Marcus A. Dahl <programkode@users.noreply.github.com>
2024-02-14 09:32:38 +0100

    Add Die-class from lecture 12

---
+++ code/lectures/_12/src/Die.java
@@ -0,0 +1,21 @@
+package lectures._12.src;
+
+
+//# Terning
+public class Die
+{
+    //# Fields
+    private final int sides;
+
+    //# Constructors
+    Die(int sides) {
+        this.sides = sides;
+    }
+
+    //# Getter-methods
+    public int getSides() {
+        return this.sides;
+    }
+
+    //# Methods
+}
