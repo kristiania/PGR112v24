a66f82c8813ef88ba627f6d75ed351783bbfa8a8
Marcus A. Dahl <programkode@users.noreply.github.com>
2024-02-14 10:06:43 +0100

    Add Forelesning12-class from lecture 12

--- code/lectures/_12/Forelesning12.java
+++ code/lectures/_12/Forelesning12.java
@@ -1,7 +1,38 @@
 package lectures._12;
 
+import java.util.ArrayList;
+
 public class Forelesning12 {
     public static void main(String[] args) {
         System.out.println("Velkommen til forelesning 12!");
+
+        //# Oppretter en terning
+        Die die = new Die(6);
+
+        //# Kaster terningen en gang
+        System.out.print("Å kaste terningen1 gang ga oss: ");
+        System.out.println(die.roll());
+
+        //# Kaste terninger flere ganger
+        double amountOfThrows = 1_000_000;
+        ArrayList<Integer> results = new ArrayList<>();
+
+        for (int i = 0; i < amountOfThrows; i ++) {
+            results.add(die.roll());
+        }
+
+        System.out.print("Antall kast gjort: ");
+        System.out.println(results.size());
+
+        int sum = 0;
+
+        for (Integer result : results) {
+            sum += result;
+        }
+
+        System.out.printf("Sum: %d%n".formatted(sum));
+
+        System.out.print("Gjennomsnitt: ");
+        System.out.printf("%f%n", sum/amountOfThrows);
     }
 }
