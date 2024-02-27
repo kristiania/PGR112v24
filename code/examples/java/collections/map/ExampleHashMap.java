6f439101b39fb4778d837310faf3ce59ab261e41
Marcus A. Dahl <programkode@users.noreply.github.com>
2024-02-27 10:16:06 +0100

    Add HashMap example code

--- code/examples/java/collections/map/ExampleHashMap.java
+++ code/examples/java/collections/map/ExampleHashMap.java
@@ -8,17 +8,20 @@ public class ExampleHashMap
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
 
-        // Et HashMap er en datastruktur
-        // hvor vi har nøkler som linker direkte til verdier
-
-        // det vil si at vi kan bruke nøklene for å hente
-        // ut verdiene vi har lagret knyttet til nøkkelen
-
         map.put("abc", 123);
         map.put("def", 456);
+        map.put("ghi", 789);
 
         System.out.println("Key %s contains the value: %d%n".formatted(
                 "abc", map.get("abc")
+        ));
+
+        System.out.println("Key %s contains the value: %d%n".formatted(
+                "def", map.get("def")
+        ));
+
+        System.out.println("Key %s contains the value: %d%n".formatted(
+                "ghi", map.get("ghi")
         ));
 
         // Vi kan loope igjennom nøklene og printe de ut:
@@ -28,19 +31,13 @@ public class ExampleHashMap
             System.out.println(key);
         }
 
-        // Eller print ut verdiene:
+        // Eller printe ut verdiene:
         System.out.println("---");
         for (int value : map.values()) {
             System.out.println(value);
         }
 
-        // Men, det som er viktig å huske på her
-        // dersom vi printer ut via en loop som disse to er:
-        // rekkefølgen er ikke noe vi kan gjøre oss avhengig av
-        // det vil si, den vil ikke liste opp nøklene eller verdiene
-        // utifra rekkefølgene de ble lagt til!
-
-        // Vi kan også hente ut verdier og tilsvarende nøkkler
+        // Vi kan også hente ut verdier og tilsvarende nøkler
         // i en og samme sleng:
         System.out.println("---");
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
