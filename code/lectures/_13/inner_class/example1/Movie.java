e4717180a46f7519dfbd309892822e41751f0c76
Marcus A. Dahl <programkode@users.noreply.github.com>
2024-02-27 10:19:39 +0100

    Create Movie class for use in lecture

---
+++ code/lectures/_13/inner_class/example1/Movie.java
@@ -0,0 +1,24 @@
+package lectures._13.inner_class.example1;
+
+
+public class Movie
+{
+    //# Fields
+    private String name;
+    private int releaseYear;
+
+
+    //# Constructor
+    public Movie(String name, int releaseYear) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+    }
+
+
+    //# Getter-methods
+
+
+    //# Setter-methods
+
+
+}
