0997d8ac62db03c08455af9b4fd2f1580dab28aa
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2024-04-17 09:15:47 +0200

    Add files for lecture 22

---
+++ code/lectures/_22/Forelesning22.java
@@ -0,0 +1,23 @@
+package lectures._22;
+
+import java.util.ArrayList;
+
+
+public class Forelesning22
+{
+    public static void main(String[] args) {
+        ArrayList<String> topics = new ArrayList<>();
+
+        topics.add("Fortsette på koden fra i går");
+        topics.add("CRUD");
+        topics.add("Lese fra fil og inn i database");
+        topics.add("- Tips til forberedelser neste uke");
+
+        System.out.println("Forelesning 2");
+
+        for (String topic : topics) {
+            System.out.print(" - ");
+            System.out.println(topic);
+        }
+    }
+}
